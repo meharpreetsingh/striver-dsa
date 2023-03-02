7a4279a1254034b00c78ab17e7f1eb9b24a6f7b1
Meharpreet Singh <meharpreet0000@gmail.com>
2023-03-03 00:52:08 +0530

    Sorting Part 1 Complete

--- Striver’s A2Z DSA CourseSheet/Step 01/Step 01.5/factorial.cpp
+++ Striver’s A2Z DSA CourseSheet/Step 01/Step 01.5/factorial.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Using Recursion
 int calcFactorial(int n)
 {
   return n == 0 ? 1 : n * calcFactorial(n - 1);
+}
+
+// Factorial using loop
+int calcFactorialSimple(int n)
+{
+  int factorial = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    factorial *= i;
+  }
+  return factorial;
 }
 
 int main()
@@ -11,6 +23,6 @@ int main()
   int number;
   cout << "Enter an integer: ";
   cin >> number;
-  cout << "Factorial: " << calcFactorial(number) << endl;
+  cout << "Factorial: " << calcFactorialSimple(number) << endl;
   return 0;
 }
