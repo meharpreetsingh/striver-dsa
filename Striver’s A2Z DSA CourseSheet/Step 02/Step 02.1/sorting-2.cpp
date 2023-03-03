859c3a1a99ee28c002bdff898644c3381f3b0168
Meharpreet Singh <meharpreet0000@gmail.com>
2023-03-03 13:58:42 +0530

    Merge Sort Complete

--- Striver’s A2Z DSA CourseSheet/Step 02/Step 02.1/sorting-2.cpp
+++ Striver’s A2Z DSA CourseSheet/Step 02/Step 02.1/sorting-2.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void mergeSort( int arr[], int low, int high ) {
-  if ( low >= high )
+void merge(vector<int> &arr, int low, int mid, int high) {
+  vector<int> temp;
+  int left = low;
+  int right = mid + 1;
+
+  // Adding Elements to Temporary Array
+  while (left <= mid && right <= high) {
+    if (arr[left] <= arr[right]) {
+      temp.push_back(arr[left]);
+      left++;
+    } else {
+      temp.push_back(arr[right]);
+      right++;
+    }
+  }
+  while (left <= mid) {
+    temp.push_back(arr[left]);
+    left++;
+  }
+  while (right <= high) {
+    temp.push_back(arr[right]);
+    right++;
+  }
+  // Adding Elements to Real Array
+  for (int i = low; i <= high; i++) {
+    arr[i] = temp[i - low];
+  }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+  if (low >= high)
     return;
 
-  int mid = ( low + high ) / 2;
-  mergeSort( arr, low, mid );
-  mergeSort( arr, mid + 1, high );
+  int mid = (low + high) / 2;
+  mergeSort(arr, low, mid);
+  mergeSort(arr, mid + 1, high);
+  merge(arr, low, mid, high);
 }
 
 int main() {
-  int arr[30] = { 4, 6, 7, 5, 7, 8, 4, 6, 9, 3, 8, 7, 9, 5, 7, 3, 8, 9, 4, 7, 8, 9, 4, 3, 7, 3, 3, 4, 7, 4 };
+  vector<int> arr = {4, 6, 7, 5, 7, 8, 4, 6, 9, 3, 8, 7, 9, 5, 7, 3, 8, 9, 4, 7, 8, 9, 4, 3, 7, 3, 3, 4, 7, 4};
 
-  for ( int i = 0; i < sizeof( arr ) / sizeof( arr[0] ); i++ ) {
+  for (int i = 0; i < arr.size(); i++) {
     cout << arr[i] << "  ";
   }
-
   cout << endl;
 
-  mergeSort( arr, 0, ( sizeof( arr ) / sizeof( arr[0] ) ) );
+  mergeSort(arr, 0, (arr.size()));
 
-  for ( int i = 0; i < sizeof( arr ) / sizeof( arr[0] ); i++ ) {
+  for (int i = 0; i < arr.size(); i++) {
     cout << arr[i] << "  ";
   }
 
